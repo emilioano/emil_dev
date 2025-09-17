ada8cfe80f46c8c87f2b9f3b575b1d4ee0491539
emilioano <emil.sjoekvist@gmail.com>
2025-09-17 14:07:06 +0200

    Hej

--- 1_todayhelper/main.py
+++ 1_todayhelper/main.py
@@ -10,64 +10,101 @@ import json
 import random
 import asyncio
 
-nav = 0
 
-# ---- Navigation enums ------
-# 0 = input prompt
-# 1 = Get coordinates based on location
-# 2 = Get forecast based on coordinates
-# 3 = Talk to AI
-# 4 = Make a song
-# 999 = Exit
-
-while nav < 999:
-
-    if nav == 0:
-        # 0 = input prompt
-        inputlocation = input('Skriv en plats (engelsk benämning): ')
-        currentlocation = inputlocation
-        mood = input('Idag känner jag mig: ')
-        music = input('Idag vill jag lyssna på: ')
-        print('----------------------------------------------------------------------------------\n')
-        #plats = 'Ludvika'
-        nav = +1
-
-    if nav == 1:
-        # 1 = Get coordinates based on location  
-        print('Hämtar koordinater baserat på input:')    
-        coordinatesstring,lat,lon=fetchcoordinates(currentlocation)
-        print(coordinatesstring)
-        input('Tryck Enter för att fortsätta')
-        nav+=1
-
-    if nav == 2:
-        # 2 = Get forecast based on coordinates
-        print('Hämtar väderparametrar baserat på koordinater:')
-        forecaststring=fetchforecast(inputlocation,lat,lon)
-        print(forecaststring) 
-        input('Tryck Enter för att fortsätta')
-        nav+=1        
-
-    if nav == 3:
-        # 3 = Talk to AI
-        #print(AIprompt(forecaststring,mood,music,location))
-        #airesulttext=''
-        airesult=AIprompt(forecaststring,mood,music,inputlocation)
-        input('Tryck Enter för att fortsätta')
-        nav+=1
+def terminalrun():
+    nav = 0
+    
+    # ---- Navigation enums ------
+    # 0 = input prompt
+    # 1 = Get coordinates based on location
+    # 2 = Get forecast based on coordinates
+    # 3 = Talk to AI
+    # 4 = Make a song
+    # 999 = Exit
+    
+    while nav < 999:
+    
+        if nav == 0:
+            # 0 = input prompt
+            inputlocation = input('Skriv en plats: ')
+            currentlocation = inputlocation
+            mood = input('Idag känner jag mig: ')
+            music = input('Idag vill jag lyssna på: ')
+            print('----------------------------------------------------------------------------------\n')
+            nav = +1
+    
+        if nav == 1:
+            # 1 = Get coordinates based on location  
+            print('Hämtar koordinater baserat på input:')    
+            coordinatesstring,lat,lon=fetchcoordinates(currentlocation)
+            print(coordinatesstring)
+            input('Tryck Enter för att fortsätta')
+            nav+=1
+    
+        if nav == 2:
+            # 2 = Get forecast based on coordinates
+            print('Hämtar väderparametrar baserat på koordinater:')
+            forecaststring=fetchforecast(inputlocation,lat,lon)
+            print(forecaststring) 
+            input('Tryck Enter för att fortsätta')
+            nav+=1        
+    
+        if nav == 3:
+            # 3 = Talk to AI
+            airesult=AIprompt(forecaststring,mood,music,inputlocation)
+            print(airesult)
+            input('Tryck Enter för att fortsätta')
+            nav+=1
+        
+    
+        if nav == 4:
+            # 4 = Make a song
+            fetchsong=fetchsong(str(airesult),music,inputlocation)
+    
+    
+    
+    
+    
+        nav = int(input('---------------------------------\nMeny:\n0. Börja om\n1. Hämta koordinater igen\n2. Hämta prognos igen\n3. Skicka till AI igen.\n4. Generera en sång igen.\n999. Avsluta\n---------------------------------'))
+        
     
 
-    if nav == 4:
-        # 4 = Make a song
-        print(airesult)
-        #print(fetchsong(airesulttext,music,location)) 
-        fechsong=fetchsong(str(airesult),music,inputlocation)      
+
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
 
 
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+
+@app.get("/")
+def read_root(request: Request):
+    return templates.TemplateResponse('index.html', {'request':request})
 
 
+@app.get("/api/fetchcoordinates")
+def fetchcoordinates1(inputlocation: str):
+    coordinatesstring,lat,lon=fetchcoordinates(inputlocation)
+    return JSONResponse(content={'coordinatesstring': coordinatesstring,'lat': lat,'lon': lon})
 
-    nav = int(input('---------------------------------\nMeny:\n0. Börja om\n1. Hämta koordinater igen\n2. Hämta prognos igen\n3. Skicka till AI igen.\n4. Generera en sång igen.\n999. Avsluta\n---------------------------------'))
+@app.get("/api/fetchforecast")
+def fetchforecast1(inputlocation: str,lat: str, lon: str):
+    forecaststring=fetchforecast(inputlocation,lat,lon)
+    return JSONResponse(content={'forecaststring': forecaststring})
+
+@app.get("/api/askai")
+def askai1(forecaststring: str,mood: str, music: str, inputlocation: str):
+    airesult=AIprompt(forecaststring,mood,music,inputlocation)
+    return JSONResponse(content={'airesult': airesult})
+
+'''
+@app.get("api/coordinates")
+    coordinatesstring,lat,lon=fetchcoordinates(currentlocation)
+    print(coordinatesstring)
+    return JSONResponse(content={'result':coordinatesstring})
+'''
     
-
-
+    
+if __name__ == "__main__":
+    terminalrun()
